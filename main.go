5a8d96829c8d2f953be05f32518b9db97dcc2c68
Sachin Rai <sachinkiranti@gmail.com>
2022-05-02 01:38:21 +0545

    feat: GO loops

--- main.go
+++ main.go
@@ -2,39 +2,35 @@ package main
 
 import (
 	"fmt"
-	"sort"
-	"strings"
 )
 
 func main() {
 
-	greeting := "Hello there friends!"
+	x := 0
 
-	// Using strings lib
-	// strings does not modify
-	// return bool
-	fmt.Println(strings.Contains(greeting, "Hello there"))
+	for x < 5 {
+		fmt.Println("Value of x is :", x)
+		x++
+	}
 
-	fmt.Println(strings.ReplaceAll(greeting, "Hello", "Hi"))
-
-	fmt.Println(strings.ToUpper(greeting))
-	fmt.Println(strings.Title(greeting))
-	fmt.Println(strings.Index(greeting, "ll"))
-	fmt.Println(strings.Split(greeting, " "))
-
-	// Using sort lib
-	// sort does modify
-	ages := []int{2, 5, 8, 1, 40, 30}
-	sort.Ints(ages)
-	fmt.Println(ages)
-
-	index := sort.SearchInts(ages, 30)
-	fmt.Println(index)
+	// Traditional For loop
+	for i := 0; i < 10; i++ {
+		fmt.Println("Value of i is :", i)
+	}
 
 	names := []string{"sachin", "rai", "kiranti", "uman", "sweata", "jharna"}
-	sort.Strings(names)
-	fmt.Println(names)
+	for name := 0; name < len(names); name++ {
+		fmt.Println(names[name])
+	}
 
-	fmt.Println(sort.SearchStrings(names, "sweata"))
+	for index, value := range names {
+		fmt.Printf("The value at index %v is %v\n", index, value)
+	}
+
+	for _, value := range names {
+		fmt.Printf("The value is %v\n", value)
+	}
+
+	fmt.Println(names)
 
 }
